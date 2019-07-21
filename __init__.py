117c8f2ab0d1e1cba360a6368edc1dc7b7071227
JackKirbyCook82 <jackcookchee@yahoo.com>
2019-07-20 18:23:52 -0700

    Updated'

---
+++ __init__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 20 2019
+@name:   Visualization Functions
+@author: Jack Kirby Cook
+
+"""
+
+import pandas as pd
+
+__version__ = "1.0.0"
+__author__ = "Jack Kirby Cook"
+__all__ = ['geoplot']
+__copyright__ = "Copyright 2018, Jack Kirby Cook"
+__license__ = ""
+
+
+def geoplot(arraytable, geotable, *args, **kwargs):
+    assert arraytable.dim == 1
+    assert arraytable.headerkeys[0] == 1
+    assert arraytable.headers['geography'] == geotable.index
+    series = arraytable.toframe('geography')
+    geodataframe = pd.concat([geotable.data, series], axis=1, sort=False)
+    
+    print(arraytable)
+    geodataframe.plot(arraytable.datakey, legend=True, figsize=(6,4))    
+
+
+
+    
+    
