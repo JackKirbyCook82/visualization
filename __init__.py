4190d735c21104c7d29569072d2be0cc63cbe066
JackKirbyCook82 <jackcookchee@yahoo.com>
2019-07-27 18:51:00 -0700

    Updated

--- __init__.py
+++ __init__.py
@@ -15,15 +15,15 @@ __copyright__ = "Copyright 2018, Jack Kirby Cook"
 __license__ = ""
 
 
-def geoplot(arraytable, geotable, *args, **kwargs):
-    assert arraytable.dim == 1
-    assert arraytable.headerkeys[0] == 1
-    assert arraytable.headers['geography'] == geotable.index
-    series = arraytable.toframe('geography')
-    geodataframe = pd.concat([geotable.data, series], axis=1, sort=False)
-    
-    print(arraytable)
-    geodataframe.plot(arraytable.datakey, legend=True, figsize=(6,4))    
+#def geoplot(arraytable, geotable, *args, **kwargs):
+#    assert arraytable.dim == 1
+#    assert arraytable.headerkeys[0] == 1
+#    assert arraytable.headers['geography'] == geotable.index
+#    series = arraytable.toframe('geography')
+#    geodataframe = pd.concat([geotable.data, series], axis=1, sort=False)
+#    
+#    print(arraytable)
+#    geodataframe.plot(arraytable.datakey, legend=True, figsize=(6,4))    
 
 
 
