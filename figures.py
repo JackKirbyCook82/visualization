1864b40ae5c887dfbd8ad437cc800728a674394f
JackKirbyCook82 <jackcookchee@yahoo.com>
2019-09-30 16:52:29 -0700

    Updated

--- figures.py
+++ figures.py
@@ -9,6 +9,8 @@ Created on Sat Jul 20 2019
 import numpy as np
 from functools import reduce
 import matplotlib.pyplot as plt
+from matplotlib.colors import Normalize
+from matplotlib import cm
 from IPython.core.display import display
 
 from utilities.strings import uppercase
@@ -19,9 +21,20 @@ __all__ = ['createplot', 'createax', 'setnames', 'setticks', 'setlabels', 'showp
 __copyright__ = "Copyright 2018, Jack Kirby Cook"
 __license__ = ""
 
+_MAPCOLOR = 'YlGn'
+_ROADCOLOR = 'DarkRed'
+_WATERCOLOR = 'DarkBlue'
+
 
 _aslist = lambda items: [items] if not isinstance(items, (list, tuple)) else list(items)
 
+def config(*args, mapcolor=_MAPCOLOR, roadcolor=_ROADCOLOR, watercolor=_WATERCOLOR, **kwargs):
+    global _MAPCOLOR
+    global _ROADCOLOR
+    global _WATERCOLOR
+    _MAPCOLOR, _ROADCOLOR, _WATERCOLOR = mapcolor, roadcolor, watercolor 
+    
+    
 def showplot(fig): display(fig)
 
 def xticks(ax): return ax.get_xticks()
@@ -44,8 +57,22 @@ def createax(fig, *args, projection=None, **kwargs):
     projection = projection.lower() if projection else projection
     ax = fig.add_subplot(*fig.layout, len(fig.axes)+1, projection=projection)
     return ax
+  
 
-
+def addcolorbar(fig, colorrange, *args, orientation='horizontal', **kwargs):
+    mincolor, maxcolor = colorrange
+    norm = Normalize(vmin=mincolor, vmax=maxcolor)
+    ncmap = cm.ScalarMappable(norm=norm, cmap=_MAPCOLOR)
+    if orientation == 'vertical':
+        fig.subplots_adjust(right=0.85, wspace=0.25, hspace=0.25)    
+        cbarax = fig.add_axes([0.9, 0.12, 0.02, 0.74])      
+    elif orientation == 'horizontal':
+        fig.subplots_adjust(bottom=0.1, wspace=0.25, hspace=0.25)    
+        cbarax = fig.add_axes([0.1, 0.05, 0.8, 0.02]) 
+    else: raise ValueError(orientation)
+    fig.colorbar(ncmap, cax=cbarax, orientation=orientation)   
+    
+      
 def setnames(ax, title=None, **names):
     axisnames = dict(x = lambda name: ax.set_xlabel(uppercase(name, withops=True)), 
                      y = lambda name: ax.set_ylabel(uppercase(name, withops=True)), 
--- plots.py
+++ plots.py
@@ -10,7 +10,6 @@ import numpy as np
 import pandas as pd
 import geopandas as gp
 
-
 __version__ = "1.0.0"
 __author__ = "Jack Kirby Cook"
 __all__ = ['mapplot', 'violinplot', 'barplot', 'lineplot', 'bar3Dplot', 'surface3Dplot']
@@ -18,11 +17,22 @@ __copyright__ = "Copyright 2018, Jack Kirby Cook"
 __license__ = ""
 
 
+_MAPCOLOR = 'YlGn'
+_ROADCOLOR = 'DarkRed'
+_WATERCOLOR = 'DarkBlue'
+
+
 _aslist = lambda items: [items] if not isinstance(items, (list, tuple)) else list(items)
+
+def config(*args, mapcolor=_MAPCOLOR, roadcolor=_ROADCOLOR, watercolor=_WATERCOLOR, **kwargs):
+    global _MAPCOLOR
+    global _ROADCOLOR
+    global _WATERCOLOR
+    _MAPCOLOR, _ROADCOLOR, _WATERCOLOR = mapcolor, roadcolor, watercolor 
 
 
 # PLOTS
-def mapplot(ax, data, *args, geo, base, alpha=0.5, border=False, **kwargs):
+def mapplot(ax, data, *args, geo, base, roads=None, water=None, **kwargs):
     assert isinstance(geo, gp.GeoDataFrame)
     assert isinstance(base, gp.GeoDataFrame)
     assert isinstance(data, pd.Series)
@@ -32,23 +42,26 @@ def mapplot(ax, data, *args, geo, base, alpha=0.5, border=False, **kwargs):
     assert not geomap.isnull().values.any()
     ax.set_aspect('equal')
     basemap.plot(ax=ax, color='white', edgecolor='black')
-    geomap.plot(ax=ax, column=data.name, alpha=alpha, edgecolor='black' if border else None)
-    
+    xlimit, ylimit = ax.get_xlim(), ax.get_ylim() 
+    geomap.plot(ax=ax, column=data.name, alpha=0.5, cmap=_MAPCOLOR, edgecolor=None)    
+    if roads: roads.plot(ax=ax, alpha=1, linewidth=0.3, edgecolor=_ROADCOLOR)
+    if water: water.plot(ax=ax, alpha=1, linewidth=0.3, edgecolor=_WATERCOLOR)
+    ax.set_xlim(xlimit)
+    ax.set_ylim(ylimit)
+
            
-def violinplot(ax, data, *args, showmeans=True, showmedians=False, showextrema=True, **kwargs):
+def violinplot(ax, data, *args, **kwargs):
     assert isinstance(data, list)
     assert all([isinstance(item, np.ndarray) for item in data])
-    assert all([isinstance(item, bool) for item in (showmeans, showmedians, showextrema)])
-    ax.violinplot(data, showmeans=showmeans, showmedians=showmedians, showextrema=showextrema)
+    ax.violinplot(data, showmeans=True, showmedians=False, showextrema=True)
 
 
-def barplot(ax, data, *args, size=0.8, **kwargs):
+def barplot(ax, data, *args, **kwargs):
     assert isinstance(data, np.ndarray)
     assert data.ndim == 1    
-    assert isinstance(size, float)
     x = np.arange(0, len(data), 1)
     z = np.zeros_like(data)
-    w = size
+    w = 0.8
     h = data    
     ax.bar(x, h, width=w, bottom=z)    
     
@@ -60,14 +73,14 @@ def lineplot(ax, data, *args, **kwargs):
     ax.plot(*data)
 
 
-def scatterplot(ax, data, *args, marker='•', **kwargs):
+def scatterplot(ax, data, *args, **kwargs):
     assert isinstance(data, list)
     assert all([isinstance(item, np.ndarray) for item in data])    
     assert len(data) <= 3
-    ax.scatter(*data, marker=marker)
+    ax.scatter(*data, marker='•')
     
 
-def bar3Dplot(ax, data, *args, size=(0.8,0.8), offset=(0,0), shade=True, alpha=0.5, **kwargs):
+def bar3Dplot(ax, data, *args, size=(0.8,0.8), offset=(0,0), **kwargs):
     assert isinstance(data, np.ndarray)
     assert data.ndim == 2        
     assert isinstance(size, tuple)
@@ -81,17 +94,17 @@ def bar3Dplot(ax, data, *args, size=(0.8,0.8), offset=(0,0), shade=True, alpha=0
     z = np.zeros_like(x + y)
     w, d = size
     h = data.flatten()
-    ax.bar3d(x-w/2, y-d/2, z, w, d, h, shade=shade, alpha=alpha)   
+    ax.bar3d(x-w/2, y-d/2, z, w, d, h, shade=True, alpha=0.5)   
 
 
-def surface3Dplot(ax, data, *args, shade=True, alpha=0.5, **kwargs):
+def surface3Dplot(ax, data, *args, **kwargs):
     assert isinstance(data, np.ndarray)
     assert data.ndim == 2       
     assert isinstance(shade, bool)
     assert isinstance(alpha, float)
     x, y = np.meshgrid(*[np.arange(0, length, 1) for length in data.shape])
     z = data
-    ax.plot_surface(x, y, z, shade=shade, alpha=alpha)      
+    ax.plot_surface(x, y, z, shade=True, alpha=0.5)      
     
     
     
